51f966206cedcc0b487c47cc8f7ac4e989c03c35
vksc <vladimir.kovalyuk@gmail.com>
2015-07-22 13:42:25 +0300

    foobar extracted to separate header

--- TMP sample/foobar.cpp
+++ TMP sample/foobar.cpp
@@ -2,62 +2,9 @@
 //
 
 #include "stdafx.h"
-#include <iostream>
-#include <string>
+#include <foobar.h>
 
 using namespace std;
-
-template <int N, bool F, bool B>
-struct outer
-{	
-};
-
-template <int N>
-struct outer <N, false, false >
-{
-	static const int value = N;
-};
-
-template <int N>
-struct outer <N, true, false >
-{
-	static const string value;
-};
-
-template <int N>
-struct outer <N, false, true>
-{
-	static const string value;
-};
-
-template <int N>
-struct outer <N, true, true>
-{
-	static const string value;
-};
-
-template<int N>
-const string outer<N, true, false>::value = "Foo";
-
-template<int N>
-const string outer<N, false, true>::value = "Bar";
-
-template<int N>
-const string outer<N, true, true>::value = "FooBar";
-
-template<int N> 
-void Print()
-{
-	Print<N - 1>();
-	
-	using out = outer<N, N%3 == 0, N%5 == 0>;
-
-	cout << out::value << endl;
-}
-
-template <>
-void Print<0>()
-{};
 
 int _tmain(int argc, _TCHAR* argv[])
 {
